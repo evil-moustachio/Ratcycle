317696549ea5b7a7bd93697c00285b01f7e9dfa5
Arjo Bruijnes <arjo@weirdwater.net>
2015-05-19 16:08:51 +0200

    Base Files for the Game

---
+++ Classes/Views/View.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+namespace Ratcycle
+{
+	public class View
+	{
+		private Game _game;
+
+		public View (Game game)
+		{
+			_game = game;
+		}
+
+		public void Update ()
+		{
+		}
+
+		public void Draw (SpriteBatch spriteBatch)
+		{
+		}
+	}
+}
+

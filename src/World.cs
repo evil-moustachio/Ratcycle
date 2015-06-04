42007402d52ceaed6e600be3ff49ad403c0b34db
MaxWinsemius <maxwinsemius@me.com>
2015-06-04 20:36:47 +0200

    Implement mouse and keyhandler
    Implement the mouse handler and key handler in world.cs.

--- src/World.cs
+++ src/World.cs
@@ -11,22 +11,27 @@ namespace Ratcycle
     {
         private Game _game;
         private ViewController _viewController;
+		private MouseHandler _mouseHandler;
+		private KeyHandler _keyHandler;
 
         public Model Model;
 
-        // World constructor
         public World(Game1 game)
         {
             _game = game;
             Model = new Model();
             _viewController = new ViewController(game);
+			_mouseHandler = new MouseHandler ();
+			_keyHandler = new KeyHandler ();
         }
 
         public void Update()
         {
-            Model.Update();
+			//Update the model
+			Model.Update();
+
+			// Update the world
             _viewController.Update();
-            // Update the world
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -34,5 +39,23 @@ namespace Ratcycle
             // Draw the world
             _viewController.Draw(spriteBatch);
         }
+
+		/// <summary>
+		/// Gets the mouse handler.
+		/// </summary>
+		/// <value>The mouse handler.</value>
+		public MouseHandler MouseHandler
+		{
+			get { return _mouseHandler; }
+		}
+
+		/// <summary>
+		/// Gets the key handler.
+		/// </summary>
+		/// <value>The key handler.</value>
+		public KeyHandler KeyHandler
+		{
+			get { return _keyHandler; }
+		}
     }
 }
